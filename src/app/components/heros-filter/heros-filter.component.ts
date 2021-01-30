32d94db8ba974c42e0baea1e875c41a05ba3da68
Alberto Padilla <nyl@live.cl>
2021-01-30 20:41:06 -0300

    Initial commit

---
+++ src/app/components/heros-filter/heros-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros-filter',
+  templateUrl: './heros-filter.component.html',
+  styleUrls: ['./heros-filter.component.css']
+})
+export class HerosFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
